721737bdc3604443caed2e7a4d866faba82f7488
NileshNSwami7 <60507169+NileshNSwami7@users.noreply.github.com>
2020-12-25 15:47:25 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,18 @@
+class Test
+{
+	public String toString()
+	{
+		return "test";
+	}
+	public static void main(String args[])
+	{
+		String s = new String("Nilesh");
+		System.out.println(s);
+		
+		Integer i = new Integer(101);
+		System.out.println(i);
+		
+		Test t = new Test();
+		System.out.println(t);
+	}
+}
